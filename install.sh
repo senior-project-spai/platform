da41273ebe418a3ffc8e4dda581f8779508750a0
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-09-30 08:57:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,6 +33,14 @@ fi
 systemctl restart docker
 systemctl enable docker
 
+# install oc CLI tool
+wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+mkdir -p /home/babe/bin
+mv /home/babe/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz /home/babe/bin/
+cd /home/babe/bin/
+tar -xf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+
+
 # install the packages for Ansible
 yum -y --enablerepo=epel install ansible pyOpenSSL
 curl -o ansible.rpm https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
