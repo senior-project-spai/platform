a8277637c261fac58a917be26ffb5012eb708979
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-09-30 09:06:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,11 +34,11 @@ systemctl restart docker
 systemctl enable docker
 
 # install oc CLI tool
-wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
-mkdir -p /home/babe/bin
-mv /home/babe/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz /home/babe/bin/
-cd /home/babe/bin/
-tar -xf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+#wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+#mkdir -p /home/babe/bin
+#mv /home/babe/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz /home/babe/bin/
+#cd /home/babe/bin/
+#tar -xf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
 
 
 # install the packages for Ansible
