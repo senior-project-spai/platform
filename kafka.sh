d7f05e87cf059ba5c73318188d1afdd63cb17d5c
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-10-12 02:16:10 +0700

    Update kafka.sh

--- kafka.sh
+++ kafka.sh
@@ -1,2 +1,10 @@
 oc new-project kafka
 
+git clone https://github.com/wurstmeister/kafka-docker.git
+
+cd kafka-docker
+
+
+
+kompose up --provider=openshift -f docker-compose.yml --build build-config --namespace=kafka
+
