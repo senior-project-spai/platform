2ffc9bc25e168c0be54a77a98aea66d0e2cd003c
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-11-17 23:14:15 +0700

    Update get_inventory_file.sh

--- install/master/get_inventory_file.sh
+++ install/master/get_inventory_file.sh
@@ -3,3 +3,4 @@ echo "Downloading inventory file to ~"
 cd ~
 wget https://raw.githubusercontent.com/senior-project-spai/platform/master/inventory_template.ini
 envsubst < inventory_template.ini > inventory.ini
+rm inventory_template.ini
