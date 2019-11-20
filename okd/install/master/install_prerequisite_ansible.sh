48a066103964feecda0bcf8705f463843af3e707
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-11-20 17:14:52 +0700

    Update install_prerequisite_ansible.sh

--- okd/install/master/install_prerequisite_ansible.sh
+++ okd/install/master/install_prerequisite_ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running ansible-playbook with prerequisites.yml config"
-ansible-playbook -i ~/inventory.ini openshift-ansible/playbooks/prerequisites.yml
+ansible-playbook -i ~/inventory.ini ${OPENSHIFT_ANSIBLE_REPO_PATH}/playbooks/prerequisites.yml
