8296f07eb16c881ec4c8ace00cfa6fe6ff5186b6
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-11-20 17:14:27 +0700

    Update clone_openshift_repo.sh

--- okd/install/master/clone_openshift_repo.sh
+++ okd/install/master/clone_openshift_repo.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # checkout openshift-ansible repository
 echo "checking out openshift repo at branch release-${OKD_VERSION}"
-OPENSHIFT_ANSIBLE_REPO_PATH=$PWD
+
 [ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git
 cd openshift-ansible 
+OPENSHIFT_ANSIBLE_REPO_PATH=$PWD
 git fetch 
 git checkout release-${OKD_VERSION}
 cd ..
