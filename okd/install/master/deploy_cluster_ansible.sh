50db446e2a61faac03a8d842e4cb9e6ee9ff62ee
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-11-20 17:14:55 +0700

    Update deploy_cluster_ansible.sh

--- okd/install/master/deploy_cluster_ansible.sh
+++ okd/install/master/deploy_cluster_ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running ansible-playbook with deploy_cluster.yml config"
-ansible-playbook -i ~/inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
+ansible-playbook -i ~/inventory.ini ${OPENSHIFT_ANSIBLE_REPO_PATH}/playbooks/deploy_cluster.yml
