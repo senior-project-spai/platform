88b2969a23bca6a9414755853e95de496b273613
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-11-20 17:06:53 +0700

    Update add_publickey_to_all_nodes.sh

--- okd/install/master/add_publickey_to_all_nodes.sh
+++ okd/install/master/add_publickey_to_all_nodes.sh
@@ -3,3 +3,6 @@ echo "Adding master's public key to all server in cluster"
 ssh-copy-id root@${OKD_MASTER_HOSTNAME}
 ssh-copy-id root@${OKD_WORKER_NODE_1_HOSTNAME}
 ssh-copy-id root@${OKD_WORKER_NODE_2_HOSTNAME}
+ssh-copy-id root@${OKD_WORKER_NODE_3_HOSTNAME}
+ssh-copy-id root@${OKD_WORKER_NODE_4_HOSTNAME}
+ssh-copy-id root@${OKD_WORKER_NODE_5_HOSTNAME}
