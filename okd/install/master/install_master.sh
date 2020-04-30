7022d043755ba4e9d0c97261e702041224bc8c48
Supakorn Wongsawang <supakorn.wo@ku.th>
2020-04-30 20:57:30 +0700

    use helm 3

--- okd/install/master/install_master.sh
+++ okd/install/master/install_master.sh
@@ -29,13 +29,13 @@ echo "adding ${OKD_USERNAME} to okd with password ${OKD_PASSWORD}"
 htpasswd -b /etc/origin/master/htpasswd ${OKD_USERNAME} ${OKD_PASSWORD}
 oc adm policy add-cluster-role-to-user cluster-admin ${OKD_USERNAME}
 
-echo "Deploying helm services"
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
-chmod +x get_helm.sh
-./get_helm.sh
-kubectl --namespace kube-system create serviceaccount tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller --upgrade
+# echo "Deploying helm"
+# curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+# chmod +x get_helm.sh
+# ./get_helm.sh
+# kubectl --namespace kube-system create serviceaccount tiller
+# kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+# helm init --service-account tiller --upgrade
 
 echo "#####################################################################"
 echo "* Your console is https://console.${DOMAIN}:${API_PORT}"
@@ -48,5 +48,10 @@ echo "$ oc login -u ${OKD_USERNAME} -p ${OKD_PASSWORD} https://console.${DOMAIN}
 echo "#####################################################################"
 
 oc login -u ${OKD_USERNAME} -p ${OKD_PASSWORD} https://console.${DOMAIN}:${API_PORT}/
+
+echo "Deploying helm3"
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get-helm-3 > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
 
 echo "Finish"
