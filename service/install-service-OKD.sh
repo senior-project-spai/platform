933ad70e7cf8bc95af12171a89f1f0acfc6c60e8
Supakorn Wongsawang <supakorn.wo@ku.th>
2020-05-02 00:15:18 +0700

    add object-result-api

--- service/install-service-OKD.sh
+++ service/install-service-OKD.sh
@@ -121,6 +121,7 @@ kubectl apply -n spai -f https://raw.githubusercontent.com/senior-project-spai/f
 kubectl apply -n spai -f https://raw.githubusercontent.com/senior-project-spai/cashier-api/master/deploy.yaml
 kubectl apply -n spai -f https://raw.githubusercontent.com/senior-project-spai/object-detection/master/deploy.yaml
 kubectl apply -n spai -f https://raw.githubusercontent.com/senior-project-spai/image-input-api/master/deploy.yaml
+kubectl apply -n spai -f https://raw.githubusercontent.com/senior-project-spai/object-result-api/master/deploy.yaml
 
 # Cleanup
 echo "Cleanup"
