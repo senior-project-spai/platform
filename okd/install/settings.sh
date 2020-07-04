5463d9aa6254c5cf18f2e1bf600c091eb4f8a498
Supakorn Wongsawang <supakorn.wo@ku.th>
2020-07-04 08:56:29 +0700

    Update settings.sh

--- okd/install/settings.sh
+++ okd/install/settings.sh
@@ -7,27 +7,27 @@ export OKD_VERSION=3.11
 
 #OKD Master Node Configuration
 export OKD_MASTER_IP=158.108.38.90
-export OKD_MASTER_HOSTNAME=athena.local
+export OKD_MASTER_HOSTNAME=athena
 
 #OKD Worker Node 1 Configuration
 export OKD_WORKER_NODE_1_IP=158.108.38.88
-export OKD_WORKER_NODE_1_HOSTNAME=a2.local
+export OKD_WORKER_NODE_1_HOSTNAME=a2
 
 #OKD Worker Node 2 Configuration
 export OKD_WORKER_NODE_2_IP=158.108.38.89
-export OKD_WORKER_NODE_2_HOSTNAME=a1.local
+export OKD_WORKER_NODE_2_HOSTNAME=a1
 
 #OKD Worker Node 3 Configuration
 export OKD_WORKER_NODE_3_IP=158.108.38.91
-export OKD_WORKER_NODE_3_HOSTNAME=a3.local
+export OKD_WORKER_NODE_3_HOSTNAME=a3
 
 #OKD Worker Node 4 Configuration
 export OKD_WORKER_NODE_4_IP=158.108.38.92
-export OKD_WORKER_NODE_4_HOSTNAME=a4.local
+export OKD_WORKER_NODE_4_HOSTNAME=a4
 
 #OKD Worker Node 5 Configuration
 export OKD_WORKER_NODE_5_IP=158.108.38.93
-export OKD_WORKER_NODE_5_HOSTNAME=a5.local
+export OKD_WORKER_NODE_5_HOSTNAME=a5
 
 #The  below setting will be used to access OKD console https://console.$DOMAIN:$API_PORT"
 #By default we can login using the URL https://console.spai.ml:8443
