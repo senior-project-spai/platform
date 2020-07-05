cd6813afd26597027a33efa6993ce6a6f68edfc5
Supakorn Wongsawang <supakorn.wo@ku.th>
2020-07-05 22:57:14 +0700

    Update NFS.sh

--- service/NFS.sh
+++ service/NFS.sh
@@ -18,9 +18,9 @@ echo "/mnt/nfsshare 158.108.0.0/16(rw,sync,no_root_squash)" | sudo tee -a /etc/e
 sudo exportfs -a
 
 echo "Creating NFS Folder for Zookeeper"
-mkdir /mnt/nfsshare/zookeeper-1
-mkdir /mnt/nfsshare/zookeeper-2
-mkdir /mnt/nfsshare/zookeeper-3
+sudo mkdir /mnt/nfsshare/zookeeper-1
+sudo mkdir /mnt/nfsshare/zookeeper-2
+sudo mkdir /mnt/nfsshare/zookeeper-3
 sudo chown nfsnobody:nfsnobody /mnt/nfsshare/zookeeper-1
 sudo chown nfsnobody:nfsnobody /mnt/nfsshare/zookeeper-2
 sudo chown nfsnobody:nfsnobody /mnt/nfsshare/zookeeper-3
@@ -29,7 +29,7 @@ sudo chmod 777 /mnt/nfsshare/zookeeper-2
 sudo chmod 777 /mnt/nfsshare/zookeeper-3
 
 echo "Creating NFS Folder for hawkular"
-mkdir /mnt/nfsshare/hawkular
+sudo mkdir /mnt/nfsshare/hawkular
 sudo chown nfsnobody:nfsnobody /mnt/nfsshare/hawkular
 sudo chmod 777 /mnt/nfsshare/hawkular
 
@@ -37,9 +37,9 @@ oc create -f https://gist.githubusercontent.com/supakornbabe/032dc2fe0b09443e3e5
 oc create -f https://gist.githubusercontent.com/supakornbabe/032dc2fe0b09443e3e5b8d2c9c69d0d3/raw/1d437e3ff7e5ed59a7a2a9c468ab2973b94742f1/nfs-hawkular.yaml
 
 echo "Creating NFS Folder for Kafka"
-mkdir /mnt/nfsshare/kafka-1
-mkdir /mnt/nfsshare/kafka-2
-mkdir /mnt/nfsshare/kafka-3
+sudo mkdir /mnt/nfsshare/kafka-1
+sudo mkdir /mnt/nfsshare/kafka-2
+sudo mkdir /mnt/nfsshare/kafka-3
 sudo chown nfsnobody:nfsnobody /mnt/nfsshare/kafka-1
 sudo chown nfsnobody:nfsnobody /mnt/nfsshare/kafka-2
 sudo chown nfsnobody:nfsnobody /mnt/nfsshare/kafka-3
@@ -49,20 +49,27 @@ sudo chmod 777 /mnt/nfsshare/kafka-3
 oc create -f https://gist.githubusercontent.com/supakornbabe/032dc2fe0b09443e3e5b8d2c9c69d0d3/raw/ed04c4e5c551ef12f2ab49a372f5b3d5ea9af8b2/nfs-kafka.yaml
 
 echo "Creating NFS Folder for mariadb"
-mkdir /mnt/nfsshare/mariadb
+sudo mkdir /mnt/nfsshare/mariadb
 sudo chown nfsnobody:nfsnobody /mnt/nfsshare/mariadb
 sudo chmod 777 /mnt/nfsshare/mariadb
-mkdir /mnt/nfsshare/mariadb-2
+sudo mkdir /mnt/nfsshare/mariadb-2
 sudo chown nfsnobody:nfsnobody /mnt/nfsshare/mariadb-2
 sudo chmod 777 /mnt/nfsshare/mariadb-2
-mkdir /mnt/nfsshare/mariadb-3
+sudo mkdir /mnt/nfsshare/mariadb-3
 sudo chown nfsnobody:nfsnobody /mnt/nfsshare/mariadb-3
 sudo chmod 777 /mnt/nfsshare/mariadb-3
 oc create -f https://gist.githubusercontent.com/supakornbabe/032dc2fe0b09443e3e5b8d2c9c69d0d3/raw/80f1b1ff962168522614e3affa7a56c0d6b2d083/nfs-mariadb.yaml
 
 echo "Creating NFS Folder for minio on all server"
-mkdir /mnt/nfsshare/minio
+sudo mkdir /mnt/nfsshare/minio
 sudo chown nfsnobody:nfsnobody /mnt/nfsshare/minio
 sudo chmod 777 /mnt/nfsshare/minio
 
 oc create -f https://gist.githubusercontent.com/supakornbabe/032dc2fe0b09443e3e5b8d2c9c69d0d3/raw/05329ed84bbcd1bf88920de5ab8bc08452134e4f/nfs-minio.yaml
+
+echo "Creating NFS Folder for grafana"
+sudo mkdir /mnt/nfsshare/grafana
+sudo chown nfsnobody:nfsnobody /mnt/nfsshare/grafana
+sudo chmod 777 /mnt/nfsshare/grafana
+
+oc create -f https://gist.githubusercontent.com/supakornbabe/032dc2fe0b09443e3e5b8d2c9c69d0d3/raw/ccfdf6b783c3f17b48962ccc0507a1155e387bf4/nfs-grafana.yaml
