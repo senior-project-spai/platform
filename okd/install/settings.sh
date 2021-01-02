474e23c56ee1d4704388f51ff5199bede3c34f59
rawit.p <rawit.p@lmwn.com>
2021-01-02 23:03:06 +0700

    change setting for new network

--- okd/install/settings.sh
+++ okd/install/settings.sh
@@ -6,33 +6,28 @@
 export OKD_VERSION=3.11
 
 #OKD Master Node Configuration
-export OKD_MASTER_IP=158.108.38.90
+export OKD_MASTER_IP=192.168.0.101
 export OKD_MASTER_HOSTNAME=athena
 
 #OKD Worker Node 1 Configuration
-export OKD_WORKER_NODE_1_IP=158.108.38.88
+export OKD_WORKER_NODE_1_IP=192.168.0.102
 export OKD_WORKER_NODE_1_HOSTNAME=a2
 
 #OKD Worker Node 2 Configuration
-export OKD_WORKER_NODE_2_IP=158.108.38.89
+export OKD_WORKER_NODE_2_IP=192.168.0.103
 export OKD_WORKER_NODE_2_HOSTNAME=a1
 
 #OKD Worker Node 3 Configuration
-export OKD_WORKER_NODE_3_IP=158.108.38.91
+export OKD_WORKER_NODE_3_IP=192.168.0.104
 export OKD_WORKER_NODE_3_HOSTNAME=a3
 
 #OKD Worker Node 4 Configuration
-export OKD_WORKER_NODE_4_IP=158.108.38.92
+export OKD_WORKER_NODE_4_IP=192.168.0.105
 export OKD_WORKER_NODE_4_HOSTNAME=a4
 
 #OKD Worker Node 5 Configuration
-export OKD_WORKER_NODE_5_IP=158.108.38.93
+export OKD_WORKER_NODE_5_IP=192.168.0.106
 export OKD_WORKER_NODE_5_HOSTNAME=a5
-
-#The  below setting will be used to access OKD console https://console.$DOMAIN:$API_PORT"
-#By default we can login using the URL https://console.spai.ml:8443
-export DOMAIN=spai.ml
-export API_PORT=8443
 
 #OKD Login Credentials
 #By default admin/admin operator will be created and can be used to login to OKD console.
